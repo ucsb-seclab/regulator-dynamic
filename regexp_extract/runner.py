e5def1ef474f7ea74d76450d83d6ea53bcce4a84
Robert McLaughlin <robert349@ucsb.edu>
2020-10-14 21:29:42 -0700

    WIP: add codeql container for broad query

--- regexp_extract/runner.py
+++ regexp_extract/runner.py
@@ -14,6 +14,8 @@ import os
 import tempfile
 import sys
 import re
+import concurrent.futures
+import urllib.parse
 
 def main():
     pg_host = os.getenv('POSTGRES_HOST')
@@ -88,21 +90,23 @@ def main():
     with open(os.path.join(output_dir, 'codeql.version'), mode='w') as fout:
         fout.write(codeql_version + '\n')
 
-    for i, d in enumerate(dirs_to_explore):
-        print(f'{i}/{len(dirs_to_explore)}')
-
+    def create_db_for(d):
         root = os.path.abspath(os.path.join(d, 'package'))
 
         out_dir = os.path.join(output_dir, os.path.split(d)[1])
+        
         # if already exists then exit
-
         if os.path.isdir(out_dir):
             print('output dir already exists, not overwriting: ', out_dir, file=sys.stderr)
-            continue
+            return
 
-        os.mkdir(out_dir)
+        # add .tmp to the output dir, so if the program is killed while working then
+        # partially-created dirs do not exist
+        out_tmpdir = out_dir + '.tmp'
 
-        db_loc = os.path.join(out_dir, 'database')
+        os.mkdir(out_tmpdir)
+
+        db_loc = os.path.join(out_tmpdir, 'database')
 
         proc1 = subprocess.run(
             [
@@ -117,11 +121,130 @@ def main():
             stderr=subprocess.PIPE,
         )
 
+        if proc1.returncode != 0:
+            sout = proc1.stdout.decode('ascii')
+            if 'No JavaScript or TypeScript code found.' in sout:
+                # expected, ignore
+                pass
+            else:
+                print('returncode was nonzero:', proc1.returncode)
+                print(sout)
+                print(proc1.stderr.decode('ascii'))
+                os.unlink(out_tmpdir)
+                return
+        else:
+            # completed successfully, move the generated dir into place
+            os.rename(out_tmpdir, out_dir)
 
-        proc1.check_returncode()
+    print('Creating databases...')
+    executor = concurrent.futures.ThreadPoolExecutor(max_workers=6)
+    futures = [executor.submit(create_db_for, d) for d in dirs_to_explore]
+    for i, _ in enumerate(concurrent.futures.as_completed(futures)):
+        print(f'{i}/{len(dirs_to_explore)}')
 
-    curr.execute('ROLLBACK')
+    print('All databases created.')
 
+    print('Sanity check, ensure all package/version are accounted-for')
+
+    db_dirs = list(map(lambda x: os.path.join(output_dir, x), os.listdir(output_dir)))
+    print(f'pre-filter, found {len(db_dirs)} dirs')
+    db_dirs = list(filter(lambda x: os.path.isdir(x), db_dirs))
+
+    print(f'Found {len(db_dirs)} dirs with a database')
+
+    if len(db_dirs) == 0:
+        print('Should have found at least 1 dir, quitting')
+        exit(1)
+
+    def do_query(db_dir):
+        query_path = '/opt/regulator/constant_regexp_string_extraction.ql'
+        bqrs_path = os.path.join(db_dir, 'constant_regexp_string_extraction.bqrs')
+        db_path = os.path.join(db_dir, 'database')
+        cache_path = '/opt/regulator/compilation_cache'
+
+        if os.path.exists(bqrs_path):
+            print('.bqrs exists, skipping... generation')
+        else:
+            proc = subprocess.run(
+                [
+                    'codeql',
+                    'query',
+                    'run',
+                    '-o', bqrs_path + '.tmp', # output
+                    '-d', db_path,   # database
+                    '-j', '4',       # threads
+                    f'--compilation-cache={cache_path}',
+                    query_path,
+                ],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+            )
+            if proc.returncode != 0:
+                sout = proc.stdout.decode('ascii')
+                print('returncode was nonzero:', proc.returncode)
+                print(sout)
+                print(proc.stderr.decode('ascii'))
+                os.unlink(bqrs_path + '.tmp')
+                return
+            else:
+                # completed successfully, move the output into place
+                os.rename(bqrs_path + '.tmp', bqrs_path)
+        
+        csv_path = os.path.join(db_dir, 'constant_regexp_string_extraction.csv')
+        proc2 = subprocess.run(
+            [
+                'codeql',
+                'bqrs',
+                'decode',
+                '-o', csv_path + '.tmp',
+                '--format=csv',
+                bqrs_path,
+            ],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+
+        if proc2.returncode != 0:
+            sout = proc2.stdout.decode('ascii')
+            print('returncode was nonzero:', proc2.returncode)
+            print(sout)
+            print(proc2.stderr.decode('ascii'))
+            os.unlink(csv_path + '.tmp')
+            return
+        else:
+            # completed successfully, move the output into place
+            os.rename(csv_path + '.tmp', csv_path)
+
+
+    # perform the query over each dir
+    print('performing query...')
+    futures = [executor.submit(do_query, d) for d in db_dirs]
+    for i, _ in enumerate(concurrent.futures.as_completed(futures)):
+        print(f'{i}/{len(dirs_to_explore)}')
+
+    # db_dir_with_package_version_id = []
+    # for db_dir in db_dirs:
+    #     # get the <package-name>_<version> part
+    #     _, pn_ver = os.path.split(db_dir)
+    #     package_id, version = pn_ver.rsplit('_', maxsplit=1)
+
+    #     package_id = urllib.parse.unquote(package_id)
+
+    #     # ensure it's in the database as package_version
+    #     curr.execute('SELECT id FROM package_version WHERE package_id=%s AND version=%s', [package_id, version])
+    #     rows = curr.fetchall()
+
+    #     if len(rows) > 1:
+    #         print(f'Found multiple package_version IDs for {package_id} - {version}')
+    #         exit(1)
+    #     if len(rows) == 0:
+    #         print(f'Found no package_version ID for {package_id} - {version}')
+    #         exit(1)
+        
+    #     print(f'found id = {rows[0]}')
+
+
+    
 
 if __name__ == '__main__':
     main()
